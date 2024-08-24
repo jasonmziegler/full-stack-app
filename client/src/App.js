0220804404524f03ade21868b22c7871ccfd048c
Jason Ziegler <jason@insightfulautomation.com>
2024-08-24 14:52:05 -0500

    import project files

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    //Make API Request
+    axios.get('http://localhost:5000/api/courses')
+    .then(response => {
+      setData(response.data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    })
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is my react app.
+      <h1>API Data</h1>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
