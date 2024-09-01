184538fadaa0db90c11deca8fa830c9393faffe8
Jason Ziegler <jason@insightfulautomation.com>
2024-09-01 18:17:50 -0500

    conditionally display Header if User

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,44 @@
 // src/components/Header.js
-import '../styles/global.css';
-import { Link } from 'react-router-dom';
+//https://teamtreehouse.com/library/react-authentication-2/display-authenticated-user-in-multiple-components
+
+import "../styles/global.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 const Header = () => {
-  return(
-<header>
-            <div className="wrap header--flex">
-                <h1 className="header--logo"><Link to="/">Courses</Link></h1>
-                <nav>
-                    <ul className="header--signedout">
-                        <li><Link to="/signup">Sign Up</Link></li>
-                        <li><Link to="/signin">Sign In</Link></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-  )
-}
+  const { user } = useContext(UserContext);
+  return (
+    <header>
+      <div className="wrap header--flex">
+        <h1 className="header--logo">
+          <Link to="/">Courses</Link>
+        </h1>
+        <nav>
+          <ul className="header--signedout">
+            {console.log("Auth User: ", user)}
+            {user ? (
+              <>
+                <li>Welcome, {user.firstName}</li>
+                <li>
+                  <Link to="/signout">Sign Out</Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/signup">Sign Up</Link>
+                </li>
+                <li>
+                  <Link to="/signin">Sign In</Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
