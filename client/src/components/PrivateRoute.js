7ec8d1a1f9fe31d0def16ae4d807cf13a5dfd829
Jason Ziegler <jason@insightfulautomation.com>
2024-09-01 20:22:20 -0500

    create PrivateRoute

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { Route, Navigate } from 'react-router-dom';
+import { UserContext } from '../context/UserContext';
+
+const PrivateRoute = ({element: Component, ...rest}) => {
+  const { user } = useContext(UserContext);
+  return (
+    <Route
+      {...rest}
+      render={(props) => 
+      user ? (
+        <Component {...props} />
+      ) : (
+        <Navigate to="/signin" replace />
+      )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
